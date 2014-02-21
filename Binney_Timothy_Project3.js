9a9a6b11d2e0b6a27aa5fedcfd8c5776446d562d
Tim Binney <tjbinney@fullsail.edu>
2014-02-20 19:31:32 -0500

    Added Procedure/JSON/Function1

--- Binney_Timothy_Project3.js
+++ Binney_Timothy_Project3.js
@@ -29,3 +29,41 @@ var recieveClassInfo = function (json2) {
 recieveClassInfo(json2);
 
 console.log("You possess skills unlike their own skills and are well trained with a sword , daggers, and a bow.");
+
+myPrompt = prompt("Would you like to prepare your sword, daggers, or bow first?")
+            if (myPrompt === "sword") {
+                console.log("You sharpen the blade of your finest longsword");
+            }
+            if (myPrompt === "daggers") {
+                console.log("You grab your daggers and strike them together.")
+            }
+            if (myPrompt === "bow") {
+            console.log("You grab your bow and make sure your string is tight and ready to let loose hell!");
+
+            };          
+            
+console.log("The party spots the dragon almost at the same time the dragon spots the party")
+console.log("The party rushes into battle and attacks the dragon!!")
+console.log("The dragon has a massive 1500 healthpool and furociously attacks the party back!")
+
+
+var getBattle = function (hitpoints) {
+    
+    while (hitpoints > 300)
+    {
+        console.log("The paladin shields the party of damage and takes 100 hp loss.")
+        console.log( "The mage hurls a frostbomb at the dragon for 200 damage.")
+        console.log( "The priest heals the tank for 100 hp.")
+        console.log( "The rogue stabs the dragon in the back for 100 damage.")
+        hitpoints = hitpoints - 300
+        console.log("The dragons health is at " + hitpoints)
+        
+    };
+    
+    return hitpoints;
+}
+
+returnValue = getBattle(1500);
+
+console.log("The dragon returns to fight till its last breathe!")
+console.log("The rogue runs in terror with whatever loot he could find.")
---
+++ json.js
@@ -0,0 +1,32 @@
+//Timothy Binney
+//SDI Project 3
+//Json Data
+
+var json2 = {
+    "companions": [
+        {
+        "job": "Paladin",
+        "level": 20,
+        "role": "Tank"},
+        {
+        "job": "Mage",
+        "level": 20,
+        "role": "DPS"
+        },   
+        {
+            "job": "Priest",
+            "level": 19,
+            "role": "Healer"
+        },
+        {
+          "job": "Rogue",
+          "level": 18,
+          "role": "DPS"},
+         ]
+        };
+        
+        
+
+
+
+
